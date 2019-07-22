87a915ae8cde8c2c9979ca1c4eabd9f85afa0763
Matthew Parker <mrparker909@gmail.com>
2019-07-22 13:52:11 -0700

    - added extractUT function to extract upper triangular elements from an array of SPD matrices

--- R/SimSPDData.R
+++ R/SimSPDData.R
@@ -1,6 +1,6 @@
 #' @title genSPDdata
 #' @description Generate random SPD data with covariates (1 covariate per SPD upper diagonal entry). Default covariate coefficient is beta_j=1. Control the signal to noise ratio via SNR. Noise is gaussian and uncorrelated. A dataframe containing N rows, and 1 column for each matrix upper diagonal entry, and 1 column for each corresponding covariate. Generates Y_dimsxdims, y1...y_(n_y), and x1...x_(n_y). The y subsript counts along rows (left to right) first, then moves down one row at a time (eg for dims=3 when the main diagonal is excluded, y1 = Y[1,2], y2=Y[1,3], y3=Y[2,3]).
-#' @return If C=NULL (no confounds), returns a data.frame with 2*n_y columns, and N rows. n_y is dims*(dims/2+1/2), or dims*(dims/2-1/2) depending on whether the main diagonal is included. Otherwise, returns a list with elements Y, X, V, P, and yx, where Y is the list of observed SPD matrices, X is the data.frame of k confounds (Nxk), V is the list of k SPD covariate matrices, P is the base point on the manifold, and yx is the data.frame of upper diagonal elements of Y (with each row corresponding to one observation Y[[i]]).
+#' @return If C=NULL (no confounds), returns a data.frame with 2*n_y columns, and N rows. n_y is dims*(dims/2+1/2), or dims*(dims/2-1/2) depending on whether the main diagonal is included. Otherwise, returns a list with elements Y, X, V, P, and yx, where Y is the array of observed SPD matrices, X is the data.frame of k confounds (Nxk), V is the list of k SPD covariate matrices, P is the base point on the manifold, and yx is the data.frame of upper diagonal elements of Y (with each row corresponding to one observation Y[[i]]).
 #' @param N       number of observations (N is the number of SPD matrices generated)
 #' @param dims    dimension of SPD matrices to generate
 #' @param maxDist maximum distance on the SPD manifold for the generated SPD matrices (measured from the identity matrix)
@@ -115,7 +115,6 @@ genSPDdata <- function(N=500, dims=5, maxDist = 1, SNR=1, includeDiagonal=F, bet
         for(col in (row):dims) {
           i=i+1
           y_df[,i] = unlist(lapply(Y, FUN = function(Y) {Y[row,col]}))
-          x_df[,i] = C%*%V
         }
       }
     } else {
@@ -127,7 +126,12 @@ genSPDdata <- function(N=500, dims=5, maxDist = 1, SNR=1, includeDiagonal=F, bet
       }
     }
 
-    return(list(Y=Y, X=C, V=V, P=P, y_upper=cbind(y_df)))
+    Yret = array(0, dim=c(dims,dims,N))
+    for(i in 1:N) {
+      Yret[,,i] = Y[[i]]
+    }
+
+    return(list(Y=Yret, X=C, V=V, P=P, y_upper=cbind(y_df)))
   }
 }
 
---
+++ R/extractUT.R
@@ -0,0 +1,34 @@
+#' @title extractUT
+#' @description extract the upper triangular elements of an array of SPDs, return data.frame, each row belongs to one observation
+#' @param Y an array of dims x dims x N SPD matrices.
+#' @param dims the dimension of the SPD matrices.
+#' @param includeDiagonal if T, will return the diagonal elements as well as the upper triangular elements.
+#' @export
+extractUT <- function(Y, dims, includeDiagonal=F) {
+  Y = MGLMRiem::aug3(Y)
+  N = dim(Y)[3]
+
+  n_y = ifelse(includeDiagonal, dims*(dims/2+1/2), dims*(dims/2-1/2))
+  y_df = data.frame(matrix(ncol=n_y,nrow=N))
+  names(y_df) <- paste0("y",1:n_y)
+
+  # extract y components
+  i=0
+  if(includeDiagonal) {
+    for(row in 1:(dims)) {
+      for(col in (row):dims) {
+        i=i+1
+        y_df[,i] = unlist(Y[row,col,])
+      }
+    }
+  } else {
+    for(row in 1:(dims-1)) {
+      for(col in (row+1):dims) {
+        i=i+1
+        y_df[,i] = unlist(Y[row,col,])
+      }
+    }
+  }
+  return(y_df)
+}
+
