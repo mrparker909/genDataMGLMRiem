98697742f7a9e0ca5cb15eee2004d10289d28f40
Matthew Parker <mrparker909@gmail.com>
2019-07-23 10:34:55 -0700

    - updated manual

--- R/SimSPDData.R
+++ R/SimSPDData.R
@@ -3,12 +3,12 @@
 #' @return If C=NULL (no confounds), returns a data.frame with 2*n_y columns, and N rows. n_y is dims*(dims/2+1/2), or dims*(dims/2-1/2) depending on whether the main diagonal is included. Otherwise, returns a list with elements Y, X, V, P, and yx, where Y is the array of observed SPD matrices, X is the data.frame of k confounds (Nxk), V is the list of k SPD covariate matrices, P is the base point on the manifold, and yx is the data.frame of upper diagonal elements of Y (with each row corresponding to one observation Y[[i]]).
 #' @param N       number of observations (N is the number of SPD matrices generated)
 #' @param dims    dimension of SPD matrices to generate
-#' @param maxDist maximum distance on the SPD manifold for the generated SPD matrices (measured from the identity matrix)
-#' @param SNR     Signal to Noise ratio (noise has standard deviation = 1/SNR)
+#' @param SNR     Signal to Noise ratio (SNR=0.25 means the noise will be up to 4 times the magnitude of the signal).
 #' @param includeDiagonal If T, will include the diagonal entries of the SPD matrix. Otherwise will only include the uppder diagonal elements.
 #' @param beta    If C is NULL, this will be used to generate data under a element-wise multivariate normal model (y1...yp ~ X*Beta), NOT a Riemann Manifold model. Vector of coefficients for the generated covariates. Default is rep(1, times=dims). If beta is too short, will be right padded with zeros. If beta is too long, will be right-truncated.
 #' @param C       If C is not NULL, this will be used to generate data under a Riemann Manifold model (Y ~ X*V), NOT a multivariate normal model. A kxN array of confounds (each row is an N-vector corresponding to a set of observations for one confound, each column corresponds to a k-vector of confound observations for the ith individual)
 #' @param P       If C is not NULL, P is the base point on the manifold from which V displaces (Y ~ exp_m(P,CV)), default is P=Identity matrix (dimension dims.
+#' @param maxDist If c is NULL, the maximum distance on the SPD manifold between response values. If C is not NULL, maximum distance on the SPD manifold for the generated SPD matrices for the covariate coefficients, V (distance measured from P).
 #' @examples
 #' set.seed(1234)
 #' g <- genSPDdata(N = 3, dims = 3, SNR = 2, C=matrix(c(1,2,1),ncol=3))
@@ -63,7 +63,6 @@ genSPDdata <- function(N=500, dims=5, maxDist = 1, SNR=1, includeDiagonal=F, bet
 
     return(list(Y=Y,yx=cbind(y_df,x_df)))
   } else {
-    #### WORKING ON THIS ####
     k <- nrow(C) # number of confounds
     if(N !=ncol(C)) stop("C must be NULL, or number of columns of C must match sample size N.") # sample size
 
@@ -102,7 +101,6 @@ genSPDdata <- function(N=500, dims=5, maxDist = 1, SNR=1, includeDiagonal=F, bet
     for(j in 1:N) {
       Ysample[,,j] = MGLMRiem::addSNR_spd(Y2[,,j],SNR)
     }
-    #########################
 
     for(i in 1:N) {
       Y[[i]] = Ysample[,,i]
