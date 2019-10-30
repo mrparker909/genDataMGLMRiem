3373ceaee5177e8b247ace8307a944de593e4f09
Matthew Parker <mrparker909@gmail.com>
2019-10-30 09:57:31 -0700

    - updated manual

--- R/SimSPDData.R
+++ R/SimSPDData.R
@@ -1,20 +1,20 @@
 #' @title genSPDdata
 #' @description Generate random SPD data with covariates (1 covariate per SPD upper diagonal entry). Default covariate coefficient is beta_j=1. Control the signal to noise ratio via SNR. Noise is gaussian and uncorrelated. A dataframe containing N rows, and 1 column for each matrix upper diagonal entry, and 1 column for each corresponding covariate. Generates Y_dimsxdims, y1...y_(n_y), and x1...x_(n_y). The y subsript counts along rows (left to right) first, then moves down one row at a time (eg for dims=3 when the main diagonal is excluded, y1 = Y[1,2], y2=Y[1,3], y3=Y[2,3]).
 #' @return If C=NULL (no confounds), returns a data.frame with 2*n_y columns, and N rows. n_y is dims*(dims/2+1/2), or dims*(dims/2-1/2) depending on whether the main diagonal is included. Otherwise, returns a list with elements Y, X, V, P, and yx, where Y is the array of observed SPD matrices, X is the data.frame of k confounds (kxN), V is the list of k SPD covariate matrices, P is the base point on the manifold, and yx is the data.frame of upper diagonal elements of Y (with each row corresponding to one observation Y[[i]]).
-#' @param N       number of observations (N is the number of SPD matrices generated)
+#' @param NUM     number of observations (NUM is the number of SPD matrices generated)
 #' @param dims    dimension of SPD matrices to generate
 #' @param SNR     Signal to Noise ratio (SNR=0.25 means the noise will be up to 4 times the magnitude of the signal).
 #' @param includeDiagonal If T, will include the diagonal entries of the SPD matrix. Otherwise will only include the uppder diagonal elements.
 #' @param beta    If C is NULL, this will be used to generate data under a element-wise multivariate normal model (y1...yp ~ X*Beta), NOT a Riemann Manifold model. Vector of coefficients for the generated covariates. Default is rep(1, times=dims). If beta is too short, will be right padded with zeros. If beta is too long, will be right-truncated.
-#' @param C       If C is not NULL, this will be used to generate data under a Riemann Manifold model (Y ~ X*V), NOT a multivariate normal model. A kxN array of confounds (each row is an N-vector corresponding to a set of observations for one confound, each column corresponds to a k-vector of confound observations for the ith individual)
-#' @param nX      If C is not NULL, this will split C into C[(nX+1):nrow(C),] (nC confounds) and X=C[1:nrow(nX),] (nX covariates).
+#' @param C       If C is not NULL, this will be used to generate data under a Riemann Manifold model (Y ~ C*Vc + X*Vx), NOT a multivariate normal model. A kxN array of confounds (each row is an N-vector corresponding to a set of observations for one confound, each column corresponds to a k-vector of confound observations for the ith individual)
+#' @param X       If X is not NULL, this will X will be the covariates used to generate data under a Riemann Manifold model (Y ~ C*Vc + X*Vx).
 #' @param P       If C is not NULL, P is the base point on the manifold from which V displaces (Y ~ exp_m(P,CV)), default is P=Identity matrix (dimension dims.
 #' @param maxDist If C is NULL, the maximum distance on the SPD manifold between response values. If C is not NULL, maximum distance on the SPD manifold for the generated SPD matrices for the covariate coefficients, V (distance measured from P).
 #' @param minDist If C is NULL, the minimum distance on the SPD manifold between response values. If C is not NULL, maximum distance on the SPD manifold for the generated SPD matrices for the covariate coefficients, V (distance measured from P).
 #' @param corr    If corr=T, then the SPD matrices generated will be correlation matrices.
 #' @examples
 #' set.seed(1234)
-#' g <- genSPDdata(N = 3, dims = 3, SNR = 2, C=matrix(c(1,2,1),ncol=3))
+#' g <- genSPDdata(NUM = 3, dims = 3, SNR = 2, C=matrix(c(1,2,1),ncol=3))
 #' g$Y[[1]]
 #' MGLMRiem::expmap_spd(g$P,g$X[1]*g$V)
 #'
